e9e7910ac44ba91cbec7a1673475e818d4b8f11b
miladmo68 <miladmo68@gmail.com>
2024-10-24 16:23:18 -0400

    first commit message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=1085c784";
+const API_URL = "https://www.omdbapi.com?apikey=1085c784";
 
 function App() {
   const [movies, SetMovies] = useState([]);
